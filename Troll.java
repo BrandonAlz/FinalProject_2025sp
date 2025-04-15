e9f32c20b671aafc8817544070f5157dcb74c9c3
toaster583 <kyle@hasenkamp.us>
2025-04-15 14:11:21 -0400

    Update Troll.java

--- Troll.java
+++ Troll.java
@@ -2,10 +2,12 @@ package characters;
 
 public class Troll extends EnemyCharacter {
 	private int moveSpeed;
-    public Spider() {
-    	String name, int health, int damage, int energy, int xpReward = super("Troll", 120, 15, 83, 50); // name, health, damage, energy, xp reward
-        setDifficulty(2);
-        this.moveSpeed = 3.5; // Trolls move slowly
+	String name, int health, int damage, int energy, int xpReward = super("Troll", 120, 15, 83, 50);
+	public static void main(String[] args) {
+		this.moveSpeed = 3.5; // Trolls move slowly
+		Troll myTroll = new Troll();
+		myTroll.scaleToPlayerLevel(3);
+		myTroll.setDifficulty(2);
         
         EnemyAbilities.Heal heal = new EnemyAbilities.Heal();
         Heal.setDescription("Heals 4 health every second/every turn, unless already at full health");

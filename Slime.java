539769c6f5d4e4211cab604f28d4b4233d058f97
toaster583 <kyle@hasenkamp.us>
2025-04-15 14:12:20 -0400

    Update Slime.java

--- Slime.java
+++ Slime.java
@@ -2,10 +2,12 @@ package characters;
 
 public class Slime extends EnemyCharacter {
 	private int moveSpeed;
-    public Spider() {
-    	String name, int health, int damage, int energy, int xpReward = super("Slime", 50, 10, 40, 30); // name, health, damage, energy, xp reward
-        setDifficulty(2);
-        this.moveSpeed = 5.5; // Slimes move average speed
+	String name, int health, int damage, int energy, int xpReward = super("Slime", 50, 10, 40, 30);
+	public static void main(String[] args) {
+		this.moveSpeed = 5.5; // Slimes move average speed
+		Slime mySlime = new Slime();
+		mySlime.scaleToPlayerLevel(3);
+		mySlime.setDifficulty(2);
     }
     
     public int getMoveSpeed() {

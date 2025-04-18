4a9067ab7e17ce79e767a0d95675dcabf6f9e832
toaster583 <kyle@hasenkamp.us>
2025-04-17 23:52:13 -0400

    Update Knight.java

--- Knight.java
+++ Knight.java
@@ -5,7 +5,7 @@ public class Knight extends PlayerCharacter {
         super(name, 60, 15, 100, 25); // name, health, damage, energy, crit
         // knight abilities
         PlayerAbilities.Dash dash = new PlayerAbilities.Dash();
-        dash.setDescription("Quick movement that avoids enemy attacks and repositions the Knight");
+        dash.setDescription("Quick movement that deals damage and repositions the knight, can be used for getaway or attack");
         addAbility(dash);
     }
 }

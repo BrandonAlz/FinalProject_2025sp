972920cf1f372fefe61764e1643a60fd81cec350
toaster583 <kyle@hasenkamp.us>
2025-04-18 00:09:13 -0400

    Update Archer.java

--- Archer.java
+++ Archer.java
@@ -4,7 +4,9 @@ public class Archer extends PlayerCharacter {
 	public Archer(String name) {
 		super(name, 25, 13, 80, 26); // name, health, damage, energy, crit
 		int projectileSpeed = 7;		
-	        // Archer abilities 
+	        // Archer's base attack is a long-range, high speed arrow
+		
+		// Archer abilities 
 	        PlayerAbilities.TripleShot tripleShot = new PlayerAbilities.TripleShot();
 	        tripleShot.setDescription("Fires a spread shot of 3 arrows, one 15 degrees higher, one 15 degrees lower.");
 	        addAbility(tripleShot);

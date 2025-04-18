3d7ac164d146e22027f7d3f1b6546bd6ef260792
toaster583 <kyle@hasenkamp.us>
2025-04-18 00:08:09 -0400

    Update Knight.java

--- Knight.java
+++ Knight.java
@@ -3,7 +3,9 @@ package characters;
 public class Knight extends PlayerCharacter {
         public Knight(String name) {
                 super(name, 60, 15, 100, 25); // name, health, damage, energy, crit
-                // knight abilities
+                // Knight's base attack will be a close-range sword swing, dealing 15 damage to all enemies in the sword swipe
+                
+                // Knight abilities
                 PlayerAbilities.Dash dash = new PlayerAbilities.Dash();
                 dash.setDescription("Quick movement that deals damage and repositions the knight, can be used for getaway or attack");
                 addAbility(dash);

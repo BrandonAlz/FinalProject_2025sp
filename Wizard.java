88519f30d43faa818ae516fd710922df306ec97e
toaster583 <kyle@hasenkamp.us>
2025-04-18 00:06:10 -0400

    Update Wizard.java

--- Wizard.java
+++ Wizard.java
@@ -2,12 +2,12 @@ package characters;
 
 public class Wizard extends PlayerCharacter {
 	public Wizard(String name) {
-        super(name, 40, 12, 120, 22); // name, health, damage, energy, crit
-        // Wizard's base attack will be a fireball with decent range, low speed, and area damage
-	
-	// Wizard abilities 
-        PlayerAbilities.Fireball fireball = new PlayerAbilities.Fireball();
-        fireball.setDescription("Launches a large fireball that damages enemies in a radius.");
-        addAbility(fireball);
+        	super(name, 40, 12, 120, 22); // name, health, damage, energy, crit
+        	// Wizard's base attack will be a fireball with decent range, low speed, and area damage
+		
+		// Wizard abilities 
+        	PlayerAbilities.Fireball fireball = new PlayerAbilities.Fireball();
+        	fireball.setDescription("Launches a large fireball that damages enemies in a radius.");
+        	addAbility(fireball);
 	}
 }

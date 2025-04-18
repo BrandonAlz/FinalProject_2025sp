1a445932a55eb33f0e91180278e879e68bf7ca4b
toaster583 <kyle@hasenkamp.us>
2025-04-17 23:57:53 -0400

    Update Knight.java

--- Knight.java
+++ Knight.java
@@ -1,11 +1,11 @@
 package characters;
 
 public class Knight extends PlayerCharacter {
-    public Knight(String name) {
-        super(name, 60, 15, 100, 25); // name, health, damage, energy, crit
-        // knight abilities
-        PlayerAbilities.Dash dash = new PlayerAbilities.Dash();
-        dash.setDescription("Quick movement that deals damage and repositions the knight, can be used for getaway or attack");
-        addAbility(dash);
-    }
+        public Knight(String name) {
+                super(name, 60, 15, 100, 25); // name, health, damage, energy, crit
+                // knight abilities
+                PlayerAbilities.Dash dash = new PlayerAbilities.Dash();
+                dash.setDescription("Quick movement that deals damage and repositions the knight, can be used for getaway or attack");
+                addAbility(dash);
+        }
 }

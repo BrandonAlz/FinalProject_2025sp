e5f9feeaf823f4bc9075d873165bd1be2eb40f4d
toaster583 <kyle@hasenkamp.us>
2025-04-17 21:09:32 -0400

    Update Knight.java

--- Knight.java
+++ Knight.java
@@ -1,27 +1,20 @@
-/**
- * 
- */
 package characters;
 
-/**
- * 
- */
 public class Knight extends PlayerCharacter {
     public Knight(String name) {
-        super(name, 60, 15, 100, 15); // name, health, damage, energy, crit
-        
-        // knigh abilities
+        super(name, 60, 15, 100, 25); // name, health, damage, energy, crit
+        // knight abilities
         PlayerAbilities.Dash dash = new PlayerAbilities.Dash();
         dash.setDescription("Quick movement that avoids enemy attacks and repositions the Knight");
         addAbility(dash);
+        boolean abilityAvailable = true;
+        long elapsedTime = 0;
+        while (true) {
+        	elapsedTime = System.currentTimeMillis() - startTime;
+            long elapsedSeconds = elapsedTime / 1000;
+        	if (elapsedSeconds > 20) {
+            abilityAvailable = true; // 20 second ability cooldown
+        	}
+        }
     }
-    
-    /**
-     * Knights have a passive ability to reduce damage taken
-     */
-  
-    
-   
-    }
-
 }

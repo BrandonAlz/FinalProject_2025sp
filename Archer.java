30e6a49d8f850f70508e2950dfbea92e14d81f82
toaster583 <kyle@hasenkamp.us>
2025-04-18 00:01:15 -0400

    Update Archer.java

--- Archer.java
+++ Archer.java
@@ -1,21 +1,12 @@
 package characters;
 
 public class Archer extends PlayerCharacter {
-
-	
-	 public Archer(String name) {
-	        super(name, 50, 20, 80, 25); // name, health, damage, energy, crit
-
+	public Archer(String name) {
+		super(name, 25, 13, 80, 26); // name, health, damage, energy, crit
+		int projectileSpeed = 7;		
 	        // Archer abilities 
 	        PlayerAbilities.TripleShot tripleShot = new PlayerAbilities.TripleShot();
-	        tripleShot.setDescription("Fires three arrows in rapid succession, dealing damage to the target.");
+	        tripleShot.setDescription("Fires a spread shot of 3 arrows, one 15 degrees higher, one 15 degrees lower.");
 	        addAbility(tripleShot);
-	
-}
-
-	        
-	    }
 	}
-
-
-
+}

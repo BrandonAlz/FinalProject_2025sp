7ff87611030f387f7b7e1a1f11c71c89ea9d014b
NicholasTorres <124694129+NicholasTorres@users.noreply.github.com>
2025-04-19 15:52:05 -0400

    Add files via upload
    Added:
    
    Npc automatic movement based on chance
    Npc collision with tiles and player
    Proper indenting for dialogue
    New oldman sprites for each direction
    Player life and heart sprites (Full, Half, Empty)
    Hearts accurately displays player life
    Events and dialogue for events
    Damage event that hurts player
    Healing event that restores full HP
    Teleport event that teleports player
    Fixed a lot of bugs like inventory
    
    Planning:
    Event to transition player to new map
    A monster and their sprites
    Snow map
    Combat is some form (Hit detection etc)

---
+++ My2DGame_2/src/main/AssetSetter.java
@@ -0,0 +1,47 @@
+package main;
+
+//import entity.NPC_OldMan;
+import entity.NPC_OldMan;
+import object.*;
+
+import java.io.IOException;
+
+public class AssetSetter {
+
+    GamePanel gp;
+
+    public AssetSetter(GamePanel gp){
+        this.gp = gp;
+    }
+
+    // Here we have our method to set an object
+    public void setObject(){
+
+        // We created 2 keys, since OBJ_Key is a child class of SuperObject we can create a new key like this
+        // We then set their placement on the map using coordinated in a map of your choice
+        // We multiply by the game panel tile size so that they can be the correct size in our world
+        gp.obj[0] = new OBJ_Door();
+        gp.obj[0].x = 21;
+        gp.obj[0].y = 20;
+
+        gp.obj[1] = new OBJ_Key();
+        gp.obj[1].x = 5;
+        gp.obj[1].y = 8;
+
+        gp.obj[2] = new OBJ_Hotdog();
+        gp.obj[2].x = 20;
+        gp.obj[2].y = 18;
+
+        gp.obj[3] = new OBJ_Chest();
+        gp.obj[3].x = 9;
+        gp.obj[3].y = 9;
+    }
+
+
+    public void setNPC(){
+        gp.npc[0] = new NPC_OldMan(gp);
+        gp.npc[0].x = 2 * gp.tileSize;
+        gp.npc[0].y = 2 * gp.tileSize;
+    }
+
+}

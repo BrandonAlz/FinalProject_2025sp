24a6781552a87c070af05d1e0f431469fe42510a
toaster583 <kyle@hasenkamp.us>
2025-04-24 16:12:39 -0400

    Update Archer.java

--- Archer.java
+++ Archer.java
@@ -5,10 +5,26 @@ public class Archer extends PlayerCharacter {
 		super(name, 25, 13, 80, 26); // name, health, damage, energy, crit
 		int projectileSpeed = 7;		
 	        // Archer's base attack is a long-range, high speed arrow
-		
-		// Archer abilities 
+		boolean archerAlive = true;
+                int lives = 3;
+                if (lives = 0) { // we can add a while true here if necessary
+                        archerAlive = false;
+                }
+                // Archer abilities 
 	        PlayerAbilities.TripleShot tripleShot = new PlayerAbilities.TripleShot();
 	        tripleShot.setDescription("Fires a spread shot of 3 arrows, one 15 degrees higher, one 15 degrees lower.");
 	        addAbility(tripleShot);
+
+                public void loseLife {
+                        lives = lives - 1;
+                }
+    
+                public int getLives {
+                        return lives;
+                }
+
+		public void isAlive {
+			return archerAlive;
+		}
 	}
 }

d0082ba2a24369b27e82249b017bfc83df54e49a
toaster583 <kyle@hasenkamp.us>
2025-04-24 16:15:12 -0400

    Update Wizard.java

--- Wizard.java
+++ Wizard.java
@@ -4,10 +4,26 @@ public class Wizard extends PlayerCharacter {
 	public Wizard(String name) {
         	super(name, 40, 12, 120, 22); // name, health, damage, energy, crit
         	// Wizard's base attack will be a fireball with decent range, low speed, and area damage
-		
-		// Wizard abilities 
+		boolean wizardAlive = true;
+                int lives = 3;
+                if (lives = 0) { // we can add a while true here if necessary
+                        wizardAlive = false;
+                }
+                // Wizard abilities 
         	PlayerAbilities.Fireball fireball = new PlayerAbilities.Fireball();
         	fireball.setDescription("Launches a large fireball that damages enemies in a radius.");
         	addAbility(fireball);
+
+                public void loseLife {
+                        lives = lives - 1;
+                }
+    
+                public int getLives {
+                        return lives;
+                }
+
+		public void isAlive {
+			return wizardAlive;
+		}
 	}
 }
